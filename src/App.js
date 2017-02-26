ea823a7d2815eccf7c37519f1746f16586c08f9a
naveedaheer <naveedaheer@gmail.com>
2017-02-26 17:46:20 +0500

    These are components in react -create -export -import -render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from "./components/Layout"
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>
-          Hiworld in React.js
-        </h1>
+        < Layout />
       </div>
     )
       
